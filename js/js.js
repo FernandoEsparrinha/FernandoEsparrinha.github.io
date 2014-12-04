82ec94d67506e61a3dd8407b4dbe4df43f968799
FernandoPinto <nando5000@live.com.pt>
2014-12-04 12:43:19 +0000

    Added improved language buttons

--- js/js.js
+++ js/js.js
@@ -30,3 +30,57 @@ function toggle(elt, on, off) {
 
     }
 }
+
+function switchLanguage(language){
+        
+    if (language == 1){
+        var en = document.getElementsByClassName("en");
+            for(var i = 0; i < en.length; i++){
+                 en[i].style.display = "none"; 
+        }
+
+        var pt = document.getElementsByClassName("pt");
+            for(var i = 0; i < pt.length; i++){
+                pt[i].style.display = "inline"; 
+        }
+
+        var hk = document.getElementsByClassName("hk");
+            for(var i = 0; i < hk.length; i++){
+                hk[i].style.display = "none";  
+        }
+    }
+
+    if (language == 2 ) {
+        var en = document.getElementsByClassName("en");
+            for(var i = 0; i < en.length; i++){
+                en[i].style.display = "none"; 
+        }
+
+        var pt = document.getElementsByClassName("pt");
+            for(var i = 0; i < pt.length; i++){
+                pt[i].style.display = "none"; 
+        }
+
+        var hk = document.getElementsByClassName("hk");
+            for(var i = 0; i < hk.length; i++){
+                hk[i].style.display = "inline";  
+        }
+    };
+
+    if (language == 3 ) {
+        var en = document.getElementsByClassName("en");
+            for(var i = 0; i < en.length; i++){
+                en[i].style.display = "inline"; 
+        }
+
+        var pt = document.getElementsByClassName("pt");
+            for(var i = 0; i < pt.length; i++){
+                pt[i].style.display = "none"; 
+        }
+
+        var hk = document.getElementsByClassName("hk");
+            for(var i = 0; i < hk.length; i++){
+                hk[i].style.display = "none";  
+        }
+    };
+}
